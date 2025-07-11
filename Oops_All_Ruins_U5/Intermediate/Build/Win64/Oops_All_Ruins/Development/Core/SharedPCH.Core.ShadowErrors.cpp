fb078be3417d28ede114adc7205c686c68032126
Allen Rader <allenrader2002@gmail.com>
2025-07-11 16:01:18 -0700

    From working on this long ago

---
+++ Oops_All_Ruins_U5/Intermediate/Build/Win64/Oops_All_Ruins/Development/Core/SharedPCH.Core.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30146
+#include "C:/Users/ROG Primary/Documents/GitHub/Oops-All-Ruins-/Oops_All_Ruins_U5/Intermediate/Build/Win64/Oops_All_Ruins/Development/Core/SharedPCH.Core.ShadowErrors.h"
